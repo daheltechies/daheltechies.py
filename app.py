4b3c1a0946b7da77c3e3741826f052f9366f337a
Dahel Techies <108152554+daheltechies@users.noreply.github.com>
2022-06-24 22:48:53 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 import streamlit as st
 import pandas as pd
 from PIL import Image
+from deta import Deta
+import json
 
+# Column For Page
 col1, col2 = st.columns(2)
+
 # Image For Page
 image = Image.open('image1.png')
 
+# Page Header
 col1.header("Dahel Techies Internship App")
 title_1 =  '<p style="font-family:sans-serif; color:Grey;">This Application allows you to submit your internship videos and files.</p>'
 col1.markdown(title_1, unsafe_allow_html=True)
@@ -14,6 +19,10 @@ col1.markdown(title_2, unsafe_allow_html=True)
 title_3 = '<p style="font-family:sans-serif; color:Grey;">Choose the cohort you belong to correctly</p>'
 col1.markdown(title_3, unsafe_allow_html=True)
 col2.image(image)
+
+# Deta Detabase
+deta = Deta(st.secrets["deta_key"])
+db = deta.Base("CRM-Records")
 
 col_1, col_2 = st.columns(2)
 
