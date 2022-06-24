bbca1596a9f514f999b048eed9c44d3f178f53be
Dahel Techies <108152554+daheltechies@users.noreply.github.com>
2022-06-24 22:25:22 +0100

    Update app.py

--- app.py
+++ app.py
@@ -15,13 +15,5 @@ col2.image(image)
 
 st.write("Please Choose The Right Cohort You Belong To")
 
-january = st.button("January Cohort")
-if january:
-        assignment = st.sidebar.selectbox("Choose:", ('Excel Internship', 'SQL Internship','Tableau Internship', 'PowerBI Internship'))
-february = st.button("January Cohort")
-else:
-    january = st.button("January Cohort")
-        
-        
 
 
