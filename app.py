02d37d4017b8f47572e7adf961c4f234f37c2492
Dahel Techies <108152554+daheltechies@users.noreply.github.com>
2022-06-24 22:39:54 +0100

    Update app.py

--- app.py
+++ app.py
@@ -11,11 +11,11 @@ title_1 =  '<p style="font-family:sans-serif; color:Grey;">This Application allo
 col1.markdown(title_1, unsafe_allow_html=True)
 title_2 = '<p style="font-family:sans-serif; color:Grey;">Kindly fill in your details correctly</p>'
 col1.markdown(title_2, unsafe_allow_html=True)
+title_3 = '<p style="font-family:sans-serif; color:Grey;">Choose the cohort you belong to correctly</p>'
+col1.markdown(title_3, unsafe_allow_html=True)
 col2.image(image)
 
 col_1, col_2 = st.columns(2)
-
-st.write('Choose the cohort you belong to correctly')
 
 january = col_1.checkbox('January Cohort')
 february = col_2.checkbox('February Cohort')
