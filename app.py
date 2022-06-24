eb57ab3a5d20718ad23cd806d8187076585ddcea
Dahel Techies <108152554+daheltechies@users.noreply.github.com>
2022-06-24 23:51:27 +0100

    Update app.py

--- app.py
+++ app.py
@@ -29,8 +29,8 @@ with st.form("internship app"):
     name = col_1.text_input('Your Name')
     cohort = col_1.text_input('Write Your Cohort Month')
     excel = col_2.text_input('Enter Excel Google Drive Folder Link (If Null Skip)')
-    sql = col_1.text_input('Enter Your SQL Google Drive Folder Link')
-    tableau = col_1.text_input('Enter Your Tableau Google Drive Folder Link')
+    sql = col_2.text_input('Enter Your SQL Google Drive Folder Link')
+    tableau = col_2.text_input('Enter Your Tableau Google Drive Folder Link')
     powerbi = col_1.text_input('Enter Your Power BI Google Drive Folder Link')
     submit = st.form_submit_button("Submit")
     if submit:
