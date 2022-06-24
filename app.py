89b4491b3936d1f0b2525fca8551219bd3f34dca
Dahel Techies <108152554+daheltechies@users.noreply.github.com>
2022-06-24 21:46:42 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,3 +5,10 @@ from PIL import Image
 col1, col2 = st.columns(2)
 # Image For Page
 image = Image.open('image1.png')
+
+col1.header("Dahel Techies Internship App")
+title_1 =  '<p style="font-family:sans-serif; color:Grey;">This Application allows you to submit your internship videos and files.</p>'
+col1.markdown(title_1, unsafe_allow_html=True)
+title_2 = '<p style="font-family:sans-serif; color:Grey;">Kindly fill in your details correctly</p>'
+col1.markdown(title_2, unsafe_allow_html=True)
+
