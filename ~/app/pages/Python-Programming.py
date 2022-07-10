04dc5ee41e14a8118504efab6277018485954450
Dahel Techies <108152554+daheltechies@users.noreply.github.com>
2022-07-10 17:39:56 +0100

    Update Python-Programming.py

--- ~/app/pages/Python-Programming.py
+++ ~/app/pages/Python-Programming.py
@@ -11,8 +11,8 @@ col3, col4 = st.columns(2)
 image = Image.open('image1.png')
 
 # Page Header
-col3.header("Dahel Techies Assignment App")
-title_4 =  '<p style="font-family:sans-serif; color:Grey;">This Application allows you to submit your Assignment files.</p>'
+col3.header("Dahel Techies Python Programming Assignment App")
+title_4 =  '<p style="font-family:sans-serif; color:Grey;">This Application allows you to submit your Python Programming Assignment files.</p>'
 col3.markdown(title_4, unsafe_allow_html=True)
 title_5 = '<p style="font-family:sans-serif; color:Grey;">Kindly fill in your details correctly</p>'
 col3.markdown(title_5, unsafe_allow_html=True)
@@ -23,17 +23,15 @@ col4.image(image)
 col_3, col_4 = st.columns(2)
 
 # Form
-deta = Deta(st.secrets["deta_key1"])
-db = deta.Base("assignment-Records")
+deta = Deta(st.secrets["deta_key3"])
+db = deta.Base("python-Records")
 
-with st.form("assignment app", clear_on_submit=True):
+with st.form("python app", clear_on_submit=True):
     name = col_3.text_input('Your Name')
     cohort = col_3.text_input('Write Your Cohort Month')
-    excel = col_4.text_input('Enter Excel Google Drive Folder Link')
-    sql = col_3.text_input('Enter Your SQL Google Drive Folder Link')
-    tableau = col_4.text_input('Enter Your Tableau Google Drive Folder Link')
-    powerbi = col_3.text_input('Enter Your Power BI Google Drive Folder Link')
+    assignment = col_4.text_input('Enter Your Assignment Google Drive Folder Link')
+    link = col_4.text_input('Enter the link of your project if available')
     submit = st.form_submit_button("Submit")
     if submit:
            st.write("Submit Successful")
-           db.put({"Name1":name, "Course":cohort, "Excel_Course":excel, "SQL_Course":sql, "Tableau_Course":tableau, "PowerBI_Course":powerbi})
+           db.put({"Name ":name, "Course":cohort, "Assignment":assignment, "Project Link":link})
